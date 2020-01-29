b9dca40cb273146e4c19b0153ca5de60c7d3ee9b
ashish007kumar <ashishkumar357ak@gmail.com>
2020-01-29 23:38:36 +0530

    136

---
+++ 136.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+               int n=nums.size();
+
+        for(int i=0;i<n-1;i++)
+        {
+           nums[i+1]=nums[i]^nums[i+1];
+        }
+        return nums[n-1];
+    }
+};
