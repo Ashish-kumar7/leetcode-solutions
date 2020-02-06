50bbff309f1fa6fec16811410c3dc6027fcbaaf1
ashish007kumar <ashishkumar357ak@gmail.com>
2020-02-06 20:43:11 +0530

    905

---
+++ 905.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+     vector<int>B;
+        for(int i=0;i<A.size();i++)
+        {
+            if(A[i]%2==0)
+            {
+            B.push_back(A[i]);                
+            }
+        }
+        for(int i=0;i<A.size();i++)
+        {
+            if(A[i]%2!=0)
+            {
+            B.push_back(A[i]);                
+            }
+        }
+        
+        return B;
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+};
