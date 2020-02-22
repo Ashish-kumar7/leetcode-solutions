261060511a059093e20db8ae6f8c3c0b55c92b42
ashish007kumar <ashishkumar357ak@gmail.com>
2020-02-22 15:49:36 +0530

    226a

---
+++ 226(a).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        
+        TreeNode *  right=invertTree(root->right);
+        TreeNode * left=invertTree(root->left);
+        
+        root->left=right;
+        root->right=left;
+        
+        return root;
+        
+    }
+};
