393af754a1eec7656cc920e88b1318e988d19eb7
ashish007kumar <ashishkumar357ak@gmail.com>
2020-02-26 23:32:35 +0530

    543

---
+++ 543.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }    
+     int option1=height(root->left)+height(root->right);
+     int option2=diameterOfBinaryTree(root->left); 
+     int option3=diameterOfBinaryTree(root->right);
+        
+        return max(option1,max(option2,option3));   
+    }
+    int height(TreeNode * root){
+        if(root==NULL){
+            return 0;
+        }
+        return 1+max(height(root->left),height(root->right)); 
+    }
+};
