e5eec08faee969a072c62741d4ec7e38b5799898
ashish007kumar <ashishkumar357ak@gmail.com>
2020-03-27 11:59:28 +0530

    20.cpp

---
+++ 20.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int i=0;
+        stack<char>S;
+        cout<<s.length();
+        while(i!=s.length()){
+         if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            S.push(s[i]); 
+         }
+        else if(s[i]==')' || s[i]=='}' || s[i]==']'){   
+            if(S.empty()){
+                return 0;
+                }
+            char z=S.top();
+            S.pop();
+            if( ( s[i]==')' && z!='(' ) || ( s[i]=='}' && z!='{' ) || ( s[i]==']' && z!='[' ) ){
+                return 0;
+            }
+        }
+            i++;
+        }
+        if(S.empty()){
+            return 1;
+        }
+        return 0;
+    }
+};
