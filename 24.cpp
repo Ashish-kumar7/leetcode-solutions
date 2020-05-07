432e4c28e6b259dda36386b3cf1afdebb43ab855
ashish007kumar <ashishkumar357ak@gmail.com>
2020-05-07 17:47:05 +0530

    24

--- 24.cpp
+++ 24.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
+    ListNode * swap(ListNode * head){
+         if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode * curr=head;
+        ListNode * Next=head->next;
+        head=head->next->next;
+        Next->next=curr;   
+        curr->next=swap(head);
+        return Next;
+    }
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL){
-            return NULL;
-        }
-        ListNode * p=head;
-        ListNode * q;
-        ListNode * y=p->next;
-        ListNode * temp;
-        while(1 && y!=NULL){
-            q=p->next;
-            temp=q->next;
-            q->next=p;
-            if(temp==NULL || temp->next==NULL){
-                p->next=temp;
-                break;
-            }
-            p->next=temp->next;
-            p=temp;   
-        }
-        if(y!=NULL){
-            return y;
-        }
-        else{
-            return p;
-        } 
+        return swap(head);
     }
 };
