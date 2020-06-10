c09e314e344c0fc447715881344a3942481db136
ashish007kumar <ashishkumar357ak@gmail.com>
2020-06-10 21:17:27 +0530

    leetcode-com-explore-challenge-card-june-leetcoding-challenge-540-week-2-june-8th-june-14th-3354.cpp

---
+++ leetcode-com-explore-challenge-card-june-leetcoding-challenge-540-week-2-june-8th-june-14th-3354.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return 0;
+        }
+        if((n & (n-1))==0){
+            return 1;
+        }
+        return 0;
+    }
+};
