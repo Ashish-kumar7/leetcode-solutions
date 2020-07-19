7e57b72b48d3735254b57bcfb24c7d7e01857e31
ashish007kumar <ashishkumar357ak@gmail.com>
2020-07-19 15:07:17 +0530

    leetcode-com-explore-challenge-card-july-leetcoding-challenge-545-week-2-july-8th-july-14th-3385.cpp

---
+++ leetcode-com-explore-challenge-card-july-leetcoding-challenge-545-week-2-july-8th-july-14th-3385.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+          if( !root ) return 0;
+        queue<pair<TreeNode*, int> > q;
+        q.push({root, 1 });
+        int ans = 0;
+        while(!q.empty() ){
+            int size = q.size();
+            if (size == 1){
+                q.push({q.front().first,1});
+                q.pop();
+            }
+            ans = max( q.back().second - q.front().second + 1, ans);
+        
+            while( size-->0 ){
+                auto node = q.front().first;
+                auto idx = q.front().second;
+                q.pop();
+                if( node->left )
+                    q.push({node->left, idx*2});
+                if( node->right )
+                    q.push({node->right, idx*2+1});
+            }
+            
+        }
+        return ans;
+    }
+};
