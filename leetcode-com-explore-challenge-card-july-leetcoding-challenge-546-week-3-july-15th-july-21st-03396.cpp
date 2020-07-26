d8ecd472a4ac9d10b782b5d536eb8944cddcda65
ashish007kumar <ashishkumar357ak@gmail.com>
2020-07-27 00:45:41 +0530

    leetcode-com-explore-challenge-card-july-leetcoding-challenge-546-week-3-july-15th-july-21st-03396.cpp

---
+++ leetcode-com-explore-challenge-card-july-leetcoding-challenge-546-week-3-july-15th-july-21st-03396.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+     if(head==NULL){
+         return NULL;
+     }  
+     if(head->val==val){
+         return removeElements(head->next,val);
+     }
+        head->next=removeElements(head->next,val);
+        return head;
+    }
+};
