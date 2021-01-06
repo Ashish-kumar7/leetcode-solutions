4c2df972654d6229131f7557c22b01b351581444
ashish007kumar <ashishkumar357ak@gmail.com>
2021-01-06 17:54:01 +0530

    cpp

---
+++ 1539.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& A, int k) {
+       int low = 0;
+       int high = A.size()-1 ;
+       int mid;
+       while (low <= high) {
+            mid = (low + high) / 2;
+            if (A[mid] - (1 + mid) < k)  
+                low = mid + 1;
+            else
+                high = mid-1;
+        }
+        return low + k;
+    }
+};
