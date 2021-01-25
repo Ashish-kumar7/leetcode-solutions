3cf298a540930104da0253b4ceab0d11c476f150
ashish007kumar <ashishkumar357ak@gmail.com>
2021-01-25 18:29:57 +0530

    cpp

---
+++ 1678.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string interpret(string command) {
+       string ans;
+        for (int i = 0; i < s.size(); ) {
+            if (s[i] == 'G') ans += 'G', ++i;
+            else if (s[i + 1] == ')') ans += 'o', i += 2;
+            else ans += "al", i += 4;
+        }
+        return ans;
+ 
+    }
+};
